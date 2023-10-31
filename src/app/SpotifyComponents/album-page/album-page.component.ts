a52c1e460c6e20dc4a46a17d45419e483baa7832
kirtann <94744255+kirtann@users.noreply.github.com>
2023-10-31 17:09:20 +0530

    completed functionality of tracks page with design and overall animations

--- src/app/SpotifyComponents/album-page/album-page.component.ts
+++ src/app/SpotifyComponents/album-page/album-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { SpotifyApiService } from 'src/app/services/spotify-api.service';
 import { TokenSharingService } from 'src/app/services/token-sharing.service';
 import * as $ from 'jquery'
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-album-page',
@@ -13,12 +14,14 @@ export class AlbumPageComponent implements OnInit {
   id:any;
   token:string = ''
   trackList:any = []
+  flag = 0;
   constructor(private route: ActivatedRoute, private spotifyService: SpotifyApiService, private tokenShared: TokenSharingService, private router: Router){
     this.token = this.tokenShared.token;
     this.id = this.route.snapshot.paramMap.get('id');
     // console.log(this.token)
   }
   ngOnInit(): void {
+      AOS.init();
       this.spotifyService.searchAlbum(this.token, this.id)
       .then(async response => {
         this.trackList = await (response.data.items)
@@ -32,11 +35,19 @@ export class AlbumPageComponent implements OnInit {
         
   }
 
-  handlePlay(url:string, track_name:string){
+  handlePlay(url:string, track_name:string, artistName:string){
     const audioPlayer = document.getElementById('audio-player') as HTMLAudioElement;
+    if(url !== null){
+      this.flag = 1;
       audioPlayer.src = url;
       audioPlayer.play();
       $('#footer-track h2').text('Now Playing: ' + track_name);
+      $('#footer-artist').text('By ' + artistName);
+    }
+    else{
+      this.flag = 0;
+      $('#footer-track h2').text(track_name + ' will made available soon');
+    }
   }
 
   handleBack(){
